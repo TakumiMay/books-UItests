dc0b7aa3e7d51746bde1d1430aa332a5d8a30bc9
Mayumi Tamura <maythermyself@gmail.com>
2021-03-23 01:06:45 -0500

    cypress added and tests created

---
+++ cypress/integration/create_book.spec.js
@@ -0,0 +1,10 @@
+describe('When the user wants to create a book', () => {
+
+    before(() => {
+
+    })
+
+    it("Then the book should be listed with the right name and author", () => {
+
+    })
+}) 
